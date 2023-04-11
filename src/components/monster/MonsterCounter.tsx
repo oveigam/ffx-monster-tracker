bf089532ca30a35ea23078a3d8d70f1087c896cd
oscar <o.veigam@gmail.com>
2023-04-11 23:20:08 +0200

    counter styling

--- src/components/monster/MonsterCounter.tsx
+++ src/components/monster/MonsterCounter.tsx
@@ -1,6 +1,18 @@
 import { signal, useComputed } from "@preact/signals";
+import type { JSXInternal } from "preact/src/jsx";
 
 const counts = signal(new Map<string, number>());
+
+const Button = ({ children, ...props }: JSXInternal.HTMLAttributes<HTMLButtonElement>) => {
+  return (
+    <button
+      class="flex h-9 w-9 items-center justify-center rounded-full bg-slate-900 font-bold text-white"
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
 
 type Props = {
   id: string;
@@ -20,10 +32,12 @@ export const MonsterCounter = ({ id }: Props) => {
   };
 
   return (
-    <div class="flex flex-1 gap-2">
-      <button onClick={() => add(-1)}>-</button>
-      <span>{value}</span>
-      <button onClick={() => add(1)}>+</button>
+    <div class="flex flex-1 items-center gap-1">
+      <Button onClick={() => add(-1)}>-</Button>
+      <span class="flex w-11 h-11 items-center justify-center rounded-full bg-slate-800 font-bold text-white">
+        {value}
+      </span>
+      <Button onClick={() => add(1)}>+</Button>
     </div>
   );
 };
