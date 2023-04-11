cc1ab17efcafb10cd4ad5c7a16526c41d71153e4
oscar <o.veigam@gmail.com>
2023-04-12 00:21:08 +0200

    counter buttons icons

--- src/components/monster/MonsterCounter.tsx
+++ src/components/monster/MonsterCounter.tsx
@@ -4,9 +4,42 @@ import { counts } from "../../state/counts";
 
 const Button = ({ children, ...props }: JSXInternal.HTMLAttributes<HTMLButtonElement>) => {
   return (
-    <button class="flex h-9 w-9 items-center justify-center rounded-full bg-slate-900 font-bold text-white" {...props}>
+    <button
+      class="flex h-9 w-9 items-center justify-center rounded-full border border-solid border-slate-800 bg-slate-900 text-white active:bg-slate-950"
+      {...props}
+    >
       {children}
     </button>
+  );
+};
+
+const AddIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="h-5 w-5"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+    </svg>
+  );
+};
+
+const MinusIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="h-5 w-5"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15" />
+    </svg>
   );
 };
 
@@ -29,11 +62,15 @@ export const MonsterCounter = ({ id }: Props) => {
 
   return (
     <div class="flex flex-1 items-center gap-1">
-      <Button onClick={() => add(-1)}>-</Button>
-      <span class="flex h-11 w-11 items-center justify-center rounded-full bg-slate-800 font-bold text-white">
+      <Button onClick={() => add(-1)}>
+        <MinusIcon />
+      </Button>
+      <span class="flex h-11 w-11 items-center justify-center rounded-full  border border-solid border-slate-700 bg-slate-800 font-bold text-white">
         {value}
       </span>
-      <Button onClick={() => add(1)}>+</Button>
+      <Button onClick={() => add(1)}>
+        <AddIcon />
+      </Button>
     </div>
   );
 };
